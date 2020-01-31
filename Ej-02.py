72c0e274cb52c017e00f19af67f233c0d4ea0e9f
Nicolás Romero <nicolas.romero@desarrollosnea.com.ar>
2020-01-31 06:25:10 -0300

    Subo todo lo que faltaba de los archivos dañados

--- Ej-02.py
+++ Ej-02.py
@@ -1,5 +1,73 @@
 #Clase 22 Condicional If, elif y else
 
+a = 8
+
+b = 9
+
+if(a>b):
+	print(a)
+elif(a<b):
+	print(b)
+else(a==b):
+	print('Ambos dos Son Iguales')
+
+#Clase 23 Bucle For
+
+colores = ['rojo','verde','azul']
+
+for color in colores:
+	print(color)
+
+cadena = "Hola Cadena"
+
+for car in cadena:
+	print(car)
+
+range(8)
+
+for num in range:
+	print(num)
+
+for num in range(3,8):
+	print(num)
+
+for num in range(3,8,2):
+	print(num)
+
+for num in range(10):
+	if (num == 5):
+		break
+	print(num)
+
+for num in range(10):
+	if(num == 6):
+		continue
+
+for num1 in range(4):
+	for num2 in range(3):
+		print(num1,num2)
+
+#Clase 24 Bucle while
+
+while (i<=10):
+	print(i)
+	i += 1
+
+while (i<=10):
+	if (valor == 6):
+		break
+	print(i)
+	i += 1
+	
+
+while (i < 10):
+	if(i==6):
+		continue
+	printi(i)
+	i += 1
+
+
+
 #Clase 025 Clases y Objetos
 
 class ClaseSilla:
